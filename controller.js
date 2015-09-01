5e7bb24243f46cb85c1a6d4ee1887cb0797b1110
Eric Williams <eric@churchwilliams.com>
2015-09-01 09:02:29 -0600

    created really basic user profile feed that hits a live API

---
+++ controller.js
@@ -0,0 +1,11 @@
+var app = angular.module('userProfiles');
+
+app.controller('MainController', function($scope, mainService) {
+
+  $scope.getUsers = function() {
+  	mainService.getUsers().then(function(res){
+  		$scope.users = res.data.data;
+  	});	
+  }
+  $scope.getUsers();
+});
